755decc171d9c1d7d21806bba6a1402ebfdfb96f
Adam Serbinski <Adam.Serbinski@ypdine.ca>
2016-04-21 16:11:45 -0400

    Make shared objects

--- Ssl.mk
+++ Ssl.mk
@@ -20,9 +20,9 @@ include $(BUILD_STATIC_LIBRARY)
 
 #######################################
 # target shared library
-##include $(CLEAR_VARS)
-##LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
-##LOCAL_C_INCLUDES := $(log_c_includes)
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
+LOCAL_C_INCLUDES := $(log_c_includes)
 
 # If we're building an unbundled build, don't try to use clang since it's not
 # in the NDK yet. This can be removed when a clang version that is fast enough
@@ -33,13 +33,13 @@ include $(BUILD_STATIC_LIBRARY)
 ##LOCAL_SDK_VERSION := 9
 ##endif
 
-##LOCAL_SHARED_LIBRARIES += libcrypto.rb
-##LOCAL_MODULE_TAGS := optional
-##LOCAL_MODULE := libssl
-##LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
-##include $(LOCAL_PATH)/Ssl-config-target.mk
-##include $(LOCAL_PATH)/android-config.mk
-##include $(BUILD_SHARED_LIBRARY)
+LOCAL_SHARED_LIBRARIES += libcrypto
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libssl
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Ssl.mk
+include $(LOCAL_PATH)/Ssl-config-target.mk
+include $(LOCAL_PATH)/android-config.mk
+include $(BUILD_SHARED_LIBRARY)
 
 #######################################
 # host shared library
